af8c50a6610fe2c635470a073e7ff9d041ccb438
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-14 12:37:33 -0500

    Actualiza ejercicio 2: Agrega validación si se ingresa números negativos de entrada

--- Almacendesc.cpp
+++ Almacendesc.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+    double totalCompra, montoAPagar, DESCUENTO, LIMITE_DESCUENTO;
+    LIMITE_DESCUENTO=1000.0;
+    DESCUENTO=0.20;
+	cout << "Ingrese el total de su compra: ";
+	cin >> totalCompra;
+    if (totalCompra > LIMITE_DESCUENTO) {
+        montoAPagar = totalCompra * (1 - DESCUENTO);
+    } else {
+        montoAPagar = totalCompra;
+    }
+    cout << "El monto a pagar es: "<<montoAPagar<<endl;
+    return 0;
+}
