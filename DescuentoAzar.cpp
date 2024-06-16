10a326573cc33128b021b8c6dc12e2d812c64607
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-16 13:16:44 -0500

    Actualiza ejercicio 4 de estructuras selectivas: Agrega una validación de entrada del total de compra

--- DescuentoAzar.cpp
+++ DescuentoAzar.cpp
@@ -4,14 +4,18 @@ int main(){
     double total, descuento, numero;
     cout<<"Ingrese el total de la compra: ";
     cin>>total;
-    cout<<"Ingrese el numero escogido al azar: ";
-    cin>>numero;
-    
-    if(numero < 74){
+    if (total<0){
+    	cout<<"Total de compra invalido"<<endl;
+    	return 0;
+	}else{
+		cout<<"Ingrese el numero escogido al azar: ";
+    	cin>>numero;
+    	if(numero < 74){
         descuento=total*0.15;
-    }else{
+    	}else{
         descuento=total*0.20;
     }
     cout<<"El descuento aplicado es: "<<descuento<<endl;
     return 0;
+	}
 }
