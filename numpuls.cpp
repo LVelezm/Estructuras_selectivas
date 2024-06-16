6c9eeb5d5f40e78b692dc908b774d87fcd0c56df
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-15 19:01:28 -0500

    Agrega el ejercicio 5 de estructuras de control selectivas

---
+++ numpuls.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double num_pulsaciones;
+    int edad;
+    char sexo;
+    cout<<"Ingrese el sexo ('F' para femenino, 'M' para masculino):"<<endl;
+    cin>> sexo;
+    cout<<"Ingrese la edad:"<<endl;
+    cin>> edad;
+    if(sexo='F'){
+        num_pulsaciones=(220-edad)/10;
+    }else if(sexo='M'){
+        num_pulsaciones=(210-edad)/10;
+    }else{
+        cout<<"Sexo no valido. Ingrese 'F' o 'M'."<<endl;
+    }
+    cout<<"Numero de pulsaciones por cada 10 segundos:"<<num_pulsaciones<<endl;
+    return 0;
+}
