a5861866cb3915231aaef41dd81c7e73f32af06b
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-17 12:34:32 -0500

    Actualiza ejercicio 8 de estructuras selectivas: Agrega un menú para iniciar el juego

--- Preguntas.cpp
+++ Preguntas.cpp
@@ -4,8 +4,14 @@ using namespace std;
 int main(){
 	SetConsoleOutputCP(CP_UTF8);
     char respuesta;
-    cout<<"¿Colon descubrio America? (Responde S o N):";
-    cin>>respuesta;
+    int opcion;
+    cout<<"Este es un juego de preguntas (seleccione 1 o 2) "<<endl;
+    cout<<"1. Jugar"<<endl;
+    cout<<"2. Salir"<<endl;
+    cin>>opcion;
+    if(opcion==1){
+    	 cout<<"¿Colon descubrio America? (Responde S o N):";
+   		 cin>>respuesta;
     if(respuesta=='S'){
         cout<<"¿El lago Titicaca es el lago navegable mas alto del mundo? (Responde S o N):";
         cin>>respuesta;
@@ -23,6 +29,14 @@ int main(){
     }else{
         cout<<"perdiste"<<endl;
     }
-    return 0;
+    return 0;	
+	}else{
+		if(opcion==2){
+			return 0;
+		}else{
+			cout<<"Opción inválida"<<endl;
+		}
+	}
+	return 0;
 }
 
