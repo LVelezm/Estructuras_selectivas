a8d425db0896dc157f2e8c82c461e7ff269e261d
lvelezm <lvelezm@unjbg.edu.pe>
2024-06-18 22:06:00 -0500

    Actualiza ejercicio 10 de estructuras selectivas: Añade un menú para iniciar el programa con 2 opciones una para iniciar otra para salir y si en dado caso se ingresa otra pues se tomará como inválida

--- añobisiesto.cpp
+++ añobisiesto.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 int main(){
 	SetConsoleOutputCP(CP_UTF8);
-    int year;
-    cout<<"Ingrese un año:";
-    cin>>year;
+    int year, opcion;
+    cout<<"Este programa determinara si un año es bisiesto o no (seleccione 1 o 2)"<<endl;
+    cout<<"1. Iniciar"<<endl;
+    cout<<"2. Salir"<<endl;
+    cin>>opcion;
+    if (opcion==1){
+    	    cout<<"Ingrese un año:";
+    		cin>>year;
     if(year%4==0){
     	if(year%100==0){
     		if(year%400==0){
@@ -20,5 +25,13 @@ int main(){
     	cout<<year<<" no es un año bisiesto."<<endl;
     }
     return 0;
+	}else{
+		if (opcion==2){
+			return 0;
+		}else{
+			cout<<"Opción inválida"<<endl;
+		}
+	}
+
 }
 
